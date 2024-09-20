5f8413a0d713d2700b759806a31222e5c3154eca
Nick-stack517 <gsatello@gmail.com>
2024-09-20 21:40:34 +0700

    Add files via upload
    Практическое задание по уроку "Базовые структуры данных"

---
+++ main.py
@@ -0,0 +1,18 @@
+# 1st program
+print(9**0.5*5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2) == (2*(2+2)))
+
+# 4th program
+stroka = '123.456'
+num = float(stroka)
+num = num*10
+num = int(num)
+print(num % 10)
+
